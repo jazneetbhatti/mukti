0223725530c521b556859d7abbf67e5c876bca34
Tirtha Chatterjee <tirtha.p.chatterjee@gmail.com>
2013-01-22 19:46:41 +0530

    Changed transition from opacity to top position

--- scripts/script.js
+++ scripts/script.js
@@ -8,26 +8,26 @@ $(document).ready(function() {
   	$('#login-button').click( function() {
   		var dialogdiv = $('#login-dialog');
   		if (dialogdiv.css('display') == 'none') {
-  			dialogdiv.css('display', 'block');
-  			$('#loginwrapper').css('display', 'block');
+  			dialogdiv.show();
+  			$('#loginwrapper').show();
   			setTimeout(function() {
-  				dialogdiv.css('opacity', 1);
+  				dialogdiv.css('top', '70px');
   			}, 20);
   		} else {
-  			dialogdiv.css('opacity', 0);
+  			dialogdiv.css('top', '-300px');
   			setTimeout(function() {
-  				dialogdiv.css('display', 'none');
-  				$('#loginwrapper').css('display', 'none');
+  				dialogdiv.show();
+  				$('#loginwrapper').hide();
   			}, 300);
   		}
   	});
   	$('#loginwrapper').click( function() {
   		var dialogdiv = $('#login-dialog');
   		if (dialogdiv.css('display') == 'block') {
-  			dialogdiv.css('opacity', 0);
+  			dialogdiv.css('top', '-300px');
   			setTimeout(function() {
-  				dialogdiv.css('display', 'none');
-  				$('#loginwrapper').css('display', 'none');
+  				dialogdiv.hide();
+  				$('#loginwrapper').hide();
   			}, 300);
   		}
   	});
